d42b4208cbc7eb77128d7a2a78cbb0c3c99d5946
andereyaa <andrewmamawi@gmail.com>
2023-11-20 17:41:12 +0300

    updated header file rylr890 configs, MACGSP6 params

--- esp32-lorawan-macgsp6-node.h
+++ esp32-lorawan-macgsp6-node.h
@@ -1,1 +1,23 @@
+#define VERSION "V.0.0.0"
 
+// REYAX RYLR890
+#define DEFAULT_TX_PIN 18
+#define DEFAULT_RX_PIN 17
+#define baudRate 115200
+#define AT_ADDRESS 1
+#define AT_NETWORK_ID 10
+#define AT_MODE 0
+#define AT_IPR 115200
+#define AT_PARAMETER "12,4,1,7"
+#define AT_BAND 868500000
+#define AT_CRFOP 10
+#define AT_PASSWORD "92A0ECEC9000DA0DCF0CAAB0ABA2E0EF"
+
+// MACGSP Parameters
+#define DEFAULT_GP_MIN 10
+#define DEFAULT_QP_GP_COUNT 10
+#define DEFAULT_HOP_COUNTER 10
+
+// RealTime Clock
+#define RTC
+#define DEFAULT_RTC_PIN
